486745bc6bcfd09867cd7934437586289a2dc968
lucasnunessg <lucasnunespacheco@gmail.com>
2024-04-26 18:26:44 -0300

    mais componentes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import MyHeader from "./components/Header";
 
+const titleProject = "Posto de Saúde - São Gabriel, RS"
+
 function App () {
 return (
 
   <div>
-  <h1> Posto de Saúde - São Gabriel, RS</h1>
+  <h1>{titleProject}</h1>
+  <hr></hr>
   <MyHeader />
   </div>
   )
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,8 +1,24 @@
-function MyHeader() {
-  return (
-<h2> Aqui ficará o nome do médico, nome de usuário e botão de sair</h2>
 
-  )
+function MyHeader() {
+
+  const data = {medic_name: 'Nome do Médico', não_sei: 'Apenas para teste'};
+  return (
+  <>
+  <div>
+      <header>{data.medic_name}
+    </header>
+  </div>
+  <div>
+    <p>{data.não_sei}</p>
+  </div>
+    <p>
+      {`${data.medic_name} 
+      - 
+        ${data.não_sei}`}
+    </p>
+  </>
+
+  );
 }
 
-export default MyHeader
+export default MyHeader;
